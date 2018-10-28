07513f6602b8914a129ff5640d344e99813a260a
Mpot7 <r-kisa@mail.ru>
2018-10-28 18:26:57 +0300

    feat: add recursion function

--- test.js
+++ test.js
@@ -1,14 +1,26 @@
-const assert = require('assert');
+const assert = require("assert");
 Object.freeze(assert);
-const sumOfOther = require('./src/sumOfOther');
+const sumOfOther = require("./src/sumOfOther");
+const recursion = require("./src/recursion");
 
-describe('Arrays', () => {
-    it('Testing sumOfOther on array', () => {
-      const sumOf = sumOfOther([2, 3, 4, 1]);	
-      assert.deepEqual(sumOf, [8, 7, 6, 9]);
-    });
-    it('Testing sumOfOther on array', () => {
-      const sumOf = sumOfOther([1, 5, 4, 7]);	
-      assert.deepEqual(sumOf, [16, 12, 13, 10]);
-    });
+describe("sumOfOther", () => {
+  it("Testing sumOfOther on array 1", () => {
+    const sumOf = sumOfOther([2, 3, 4, 1]);	
+    assert.deepEqual(sumOf, [8, 7, 6, 9]);
+  });
+  it("Testing sumOfOther on array 2", () => {
+    const sumOf = sumOfOther([1, 5, 4, 7]);	
+    assert.deepEqual(sumOf, [16, 12, 13, 10]);
+  });
+});
+
+describe("Recursion", () => {
+  it("Testing recursion on tree 1", () => {
+    const tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
+    assert.deepEqual(recursion(tree), [[100], [90, 120], [70,99,110,130]]);
+  });
+  it("Testing recursion on tree 2", () => {
+    const tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":109}}};
+    assert.deepEqual(recursion(tree), [[100], [90, 120], [70,99,109]]);
+  });
 });
